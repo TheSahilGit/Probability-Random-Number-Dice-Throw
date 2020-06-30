289988e886cb6bd1412ec7e84d054beef46614c4
Sahil Islam <thesahil.islam@gmail.com>
2020-06-30 21:28:10 +0530

    Update DiceRolling.py

--- DiceRolling.py
+++ DiceRolling.py
@@ -7,7 +7,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 ones = []
 twos = []
 threes = []
@@ -16,7 +15,13 @@ fives = []
 sixes = []
 xs = []
 
-throws = 1000
+ratio12 = []
+ratio23 = []
+ratio34 = []
+ratio45 = []
+ratio56 = []
+ratio61 = []
+
 probability = 1 / 6.
 
 o = 0
@@ -27,28 +32,28 @@ fi = 0
 s = 0
 x = 0
 
-for i in range(throws):
-    a = np.random.random()
+maxThrow = 5000
+for throw in range(100, maxThrow, 100):
+    for i in range(throw):
+        a = np.random.random()
 
-    if 0 * probability < a < 1 * probability:
-        o += 1
+        if 0 * probability < a < 1 * probability:
+            o += 1
 
-    elif probability < a < 2 * probability:
-        tw += 1
+        if probability < a < 2 * probability:
+            tw += 1
 
-    elif 2*probability < a < 3 * probability:
-        th += 1
+        if 2 * probability < a < 3 * probability:
+            th += 1
 
-    elif 3 * probability < a < 4 * probability:
-        fo += 1
+        if 3 * probability < a < 4 * probability:
+            fo += 1
 
-    elif 4 * probability < a < 5 * probability:
-        fi += 1
+        if 4 * probability < a < 5 * probability:
+            fi += 1
 
-    elif 5 * probability < a < 6 * probability:
-        s += 1
-
-    x += 1
+        if 5 * probability < a < 6 * probability:
+            s += 1
 
     ones.append(o)
     twos.append(tw)
@@ -56,13 +61,55 @@ for i in range(throws):
     fours.append(fo)
     fives.append(fi)
     sixes.append(s)
-    xs.append(x)
+
+    xs.append(throw)
+    ratio12.append(o / tw)
+    ratio23.append(tw / th)
+    ratio34.append(th / fo)
+    ratio45.append(fo / fi)
+    ratio56.append(fi / s)
+    ratio61.append(s / o)
 
 
-plt.plot(xs, ones)
-plt.plot(xs, twos)
-plt.plot(xs, threes)
-plt.plot(xs, fours)
-plt.plot(xs, fives)
-plt.plot(xs, sixes)
-plt.show()
+def ratioPlot():
+    plt.subplot(2, 3, 1)
+    plt.plot(xs, ratio12)
+    plt.xlabel("No of total throws")
+    plt.ylabel("Ratio of ones and twos")
+    plt.grid()
+
+    plt.subplot(2, 3, 2)
+    plt.plot(xs, ratio23)
+    plt.xlabel("No of total throws")
+    plt.ylabel("Ratio of twos and threes")
+    plt.grid()
+
+    plt.subplot(2, 3, 3)
+    plt.plot(xs, ratio34)
+    plt.xlabel("No of total throws")
+    plt.ylabel("Ratio of threes and fours")
+    plt.grid()
+
+    plt.subplot(2, 3, 4)
+    plt.plot(xs, ratio45)
+    plt.xlabel("No of total throws")
+    plt.ylabel("Ratio of fours and fives")
+    plt.grid()
+
+    plt.subplot(2, 3, 5)
+    plt.plot(xs, ratio56)
+    plt.xlabel("No of total throws")
+    plt.ylabel("Ratio of fives and sixes")
+    plt.grid()
+
+    plt.subplot(2, 3, 6)
+    plt.plot(xs, ratio61)
+    plt.xlabel("No of total throws")
+    plt.ylabel("Ratio of sixes and ones")
+    plt.grid()
+
+    plt.suptitle(" Dice Throw \n Ratio of different outcomes for different no of total throws")
+    plt.subplots_adjust(0.09, 0.11, 0.95, 0.90, 0.42, 0.33)
+    plt.show()
+
+
